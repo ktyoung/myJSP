52f72e2f1d68504142bb23dafaeddc7a568a49f7
ktyoung <ktyoung0507@gmail.com>
2022-09-06 12:00:02 +0900

    Second Servlet

---
+++ pro05/src/sec01/exam01/ThirdServlet.java
@@ -0,0 +1,27 @@
+package sec01.exam01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/* 프로그램의 소스코드 안에 다른 프로그램을 위한 정보를 미리 약속된 형식으로 포함시킨 것이 어노테이션 */
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ThirdServlet init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("ThirdServlet destroy 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+}
