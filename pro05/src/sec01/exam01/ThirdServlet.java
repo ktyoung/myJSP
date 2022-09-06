a3235a9d44ac24bcb610716cb1bad7d7347fdc5f
ktyoung <ktyoung0507@gmail.com>
2022-09-06 12:03:54 +0900

    Third Servlet

--- pro05/src/sec01/exam01/ThirdServlet.java
+++ pro05/src/sec01/exam01/ThirdServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/* 프로그램의 소스코드 안에 다른 프로그램을 위한 정보를 미리 약속된 형식으로 포함시킨 것이 어노테이션 */
+/* 어노테이션 : 프로그램의 소스코드 안에 다른 프로그램을 위한 정보를 미리 약속된 형식으로 포함시킨 것 */
 @WebServlet("/third")
 public class ThirdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
