7314bd9f3d977b4b39405e8accbb09d656cc0116
ktyoung <ktyoung0507@gmail.com>
2022-09-08 15:36:54 +0900

    Gugudan If

---
+++ pro06/src/sec04/exam01/guguTest2.java
@@ -0,0 +1,58 @@
+package sec04.exam01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/guguTest2")
+public class guguTest2 extends HttpServlet {
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		int dan = Integer.parseInt(request.getParameter("dan"));
+		
+		out.print("<table border=1 width=800 align=center>");
+		out.print("<tr align=center bgcolor='#ffff66'>");
+		out.print("<td colspan=2>" + dan + " 단 출력 </td>");
+		out.print("</tr>");
+		
+		for(int i=1; i<10; i++) {
+			/*if문을 사용해 행을 나타내는 <tr>태그에 대해 교대로 다른 배경색을 적용*/
+			/*out.print("<tr align=center>");*/
+			if(i%2==0) {
+				out.print("<tr align=center bgcolor='#acfa58'>");
+			} else {
+				out.print("<tr align=center bgcolor='#81bef7'>");
+			}
+			out.print("<td width=400>");
+			out.print(dan + " * " + i);
+			out.print("</td>");
+			out.print("<td width=400>");
+			out.print(dan*i);
+			out.print("</td>");
+			out.print("</tr>");
+			
+		}
+		out.print("</table>");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+	
+	
+}
