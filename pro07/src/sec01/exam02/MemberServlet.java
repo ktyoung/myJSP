eab6b5784a88e40905eaa5dcd76debde9ab93bf1
ktyoung <ktyoung0507@gmail.com>
2022-09-14 11:34:01 +0900

    Member Servlet 2

--- pro07/src/sec01/exam02/MemberServlet.java
+++ pro07/src/sec01/exam02/MemberServlet.java
@@ -1,4 +1,4 @@
-package sec01.exam01;
+package sec01.exam02;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,19 +11,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// @WebServlet("/member")
+@WebServlet("/member")
 public class MemberServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		
-		/* 필요한 JAR 파일(드라이버)은 오라클 홈페이지에서 직접 다운받아 WEB-INF 하위 lib 폴더에 추가한다. */
-		// SQL문으로 조회할 MemberDAO 객체를 생성 
 		MemberDAO dao = new MemberDAO();
-		// listMembers() 메서드로 회원 정보를 조회
-		// 리스트와 배열의 차이? 리스트는 데이터의 타입에서 자유롭다.
 		List<MemberVO> list = dao.listMembers();
 		
 		out.print("<html><body>");
