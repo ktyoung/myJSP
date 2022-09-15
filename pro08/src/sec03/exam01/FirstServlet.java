642d5d720fcb29930a7b524e25b1de168e91e957
ktyoung <ktyoung0507@gmail.com>
2022-09-15 12:47:12 +0900

    RequestDispatcher

--- pro08/src/sec03/exam01/FirstServlet.java
+++ pro08/src/sec03/exam01/FirstServlet.java
@@ -1,22 +1,26 @@
-package sec02.exam01;
+package sec03.exam01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Time;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.RepaintManager;
 
-// @WebServlet("/first")
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		PrintWriter out = response.getWriter();
-		response.sendRedirect("second?name=lee");
+
+		RequestDispatcher dispatch = request.getRequestDispatcher("second?name=lee");
+		dispatch.forward(request, response);
 	}
 }
--- pro08/src/sec03/exam01/SecondServlet.java
+++ pro08/src/sec03/exam01/SecondServlet.java
@@ -1,8 +1,7 @@
-package sec02.exam01;
+package sec03.exam01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Time;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// @WebServlet("/first")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		PrintWriter out = response.getWriter();
-		response.sendRedirect("second?name=lee");
+		
+		out.println("<html><body>");
+		String name=request.getParameter("name");
+		out.println("이름 : " + name);
+		out.println("<br>");
+		out.println("RequestDispatcher 클래스의 forword() 메서드를 이용한 dispatch 실습");
+		out.println("</html></body>");	
 	}
 }
