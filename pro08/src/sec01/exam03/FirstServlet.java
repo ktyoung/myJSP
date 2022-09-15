684173c4fe8bfc2465998970a75bcd6d53db565c
ktyoung <ktyoung0507@gmail.com>
2022-09-15 12:07:20 +0900

    location.href

--- pro08/src/sec01/exam03/FirstServlet.java
+++ pro08/src/sec01/exam03/FirstServlet.java
@@ -1,4 +1,4 @@
-package sec01.exam02;
+package sec01.exam03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,15 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// @WebServlet("/first")
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		PrintWriter out = response.getWriter();
-		response.addHeader("Refresh","5;url=second");
-		out.println("5초 후 페이지 이동");
-
+		out.print("<script>");
+		out.print("location.href='second';");
+		out.print("</script>");
 	}
 }
--- pro08/src/sec01/exam03/SecondServlet.java
+++ pro08/src/sec01/exam03/SecondServlet.java
@@ -1,8 +1,7 @@
-package sec01.exam02;
+package sec01.exam03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Time;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// @WebServlet("/first")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8;");
 		PrintWriter out = response.getWriter();
-		response.addHeader("Refresh","5;url=second");
-		out.println("5초 후 페이지 이동");
-
+		
+		out.println("<html><body>");
+		out.println("location의  href를 이용한 실습");
+		out.println("</html></body>");	
 	}
 }
