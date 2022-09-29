840320c58952f1d6d1813ecb091c789f6f88a206
ktyoung <ktyoung0507@gmail.com>
2022-09-29 12:09:10 +0900

    List Members 3

--- pro17/src/sec02/exam02/MemberController.java
+++ pro17/src/sec02/exam02/MemberController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("*.do")
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	MemberDAO memberDAO;
